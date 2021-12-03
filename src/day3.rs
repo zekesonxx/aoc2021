3a13dc4fac85e6d8bf0679bd9c662d6215011256
Zoey Mertes <zmertes@purdue.edu>
2021-12-03 01:39:46 -0500

    Day 3 cleanups

--- src/day3.rs
+++ src/day3.rs
@@ -24,20 +24,7 @@ pub fn popularity(input: &[String]) -> Vec<usize> {
 #[aoc(day3, part1)]
 pub fn part1(input: &[String]) -> usize {
 	let lines = input.len();
-	let width = input[0].len();
-	let mut popularity = vec![];
-	for _ in 0..width {
-		popularity.push(0usize);
-	}
-	for line in input {
-		let mut chars = line.char_indices();
-		while let Some((i, x)) = chars.next() {
-			if x == '1' {
-				popularity[i] += 1;
-			}
-		}
-	}
-	println!("popularity: {:?}, lines: {}", popularity, lines);
+	let popularity = popularity(&input);
 	let mut gamma = 0;
 	let mut epsilon = 0;
 	for digit in popularity {
@@ -55,43 +42,34 @@ pub fn part1(input: &[String]) -> usize {
 
 #[aoc(day3, part2)]
 pub fn part2(input: &[String]) -> usize {
-	let width = input[0].len();
-	
-	let mut oxypop;
-	let mut oxygen = input.to_vec();
 	let mut digit = 0;
+	
+	let mut pop;
+	let mut oxygen = input.to_vec();
 	while oxygen.len() > 1 {
-		oxypop = popularity(&oxygen);
-		println!("{} ? {}", oxypop[digit], oxygen.len()/2);
-		if oxypop[digit] >= oxygen.len()-oxypop[digit] {
+		pop = popularity(&oxygen);
+		if pop[digit] >= oxygen.len()-pop[digit] {
 			oxygen.drain_filter(|x| x.chars().nth(digit).unwrap() == '0');
 		} else {
 			oxygen.drain_filter(|x| x.chars().nth(digit).unwrap() == '1');
 		}
-		println!("{:?}", oxygen);
 		digit += 1;
 	}
-	println!("oxygen: {}", oxygen[0]);
 	let oxygen = usize::from_str_radix(&oxygen[0], 2).unwrap();
-	println!("oxygen: {}", oxygen);
+	
 	digit = 0;
 	
-	let mut co2pop;
 	let mut co2 = input.to_vec();
 	while co2.len() > 1 {
-		co2pop = popularity(&co2);
-		println!("{} ? {}", co2pop[digit], co2.len()/2);
-		if co2pop[digit] < co2.len()-co2pop[digit] {
+		pop = popularity(&co2);
+		if pop[digit] < co2.len()-pop[digit] {
 			co2.drain_filter(|x| x.chars().nth(digit).unwrap() == '0');
 		} else {
 			co2.drain_filter(|x| x.chars().nth(digit).unwrap() == '1');
 		}
-		println!("{:?}", co2);
 		digit += 1;
 	}
-	println!("co2: {}", co2[0]);
 	let co2 = usize::from_str_radix(&co2[0], 2).unwrap();
-	println!("co2: {}", co2);
 
 	oxygen*co2
 }
