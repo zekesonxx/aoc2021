3d5fb1aa8995c67a647d694ee6e64b580ae7e1d6
Zoey Mertes <zmertes@purdue.edu>
2021-12-06 00:32:56 -0500

    Clean day 6 solution

--- src/day6.rs
+++ src/day6.rs
@@ -6,43 +6,33 @@ pub fn gen(input: &str) -> Vec<usize> {
 
 #[aoc(day6, part1)]
 pub fn part1(input: &[usize]) -> usize {
-	let mut fish = input.to_vec();
-	let mut newfish = 0;
-	for day in 0..80 {
-		for f in &mut fish {
-			match f {
-				1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 => *f -= 1,
-				0 => { *f = 6; newfish += 1; },
-				_ => {}
-			}
-		}
-		while newfish > 0 {
-			newfish -= 1;
-			fish.push(8);
-		}
+	let mut fish = [0usize; 9];
+	for f in input {
+		fish[*f] += 1;
 	}
-	fish.len()
+	for day in 0..80 {
+		// 0 1 2 3 4 5 6 7 8
+		// 1 2 3 4 5 6 7 8 0
+		fish.rotate_left(1);
+		fish[6] += fish[8];
+	}
+	fish.iter().sum()
 }
 
 #[aoc(day6, part2)]
 pub fn part2(input: &[usize]) -> usize {
-	let mut fish = input.to_vec();
-	let mut newfish = 0;
-	for day in 0..256 {
-		for f in &mut fish {
-			match f {
-				1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 => *f -= 1,
-				0 => { *f = 6; newfish += 1; },
-				_ => {}
-			}
-		}
-		while newfish > 0 {
-			newfish -= 1;
-			fish.push(8);
-		}
-		println!("{}", day);
+	let mut fish = [0usize; 9];
+	for f in input {
+		fish[*f] += 1;
 	}
-	fish.len()
+	for day in 0..256 {
+		// 0 1 2 3 4 5 6 7 8
+		// 1 2 3 4 5 6 7 8 0
+		fish.rotate_left(1);
+		fish[6] += fish[8];
+	}
+	fish.iter().sum()
+	
 }
 
 #[cfg(test)]
